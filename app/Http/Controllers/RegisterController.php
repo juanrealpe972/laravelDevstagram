59d64c0b5d167a5137583a07f4ba332429ac8254
juancamilo972 <juancamilo972@gmail.com>
2023-11-16 16:39:35 -0500

    insertando datos y validando datos para no ripit

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,21 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
 
-class RegisterController extends Controller{
+class RegisterController extends Controller
+{
 
-    public function index(){
+    public function index()
+    {
         //dd("#Bloquiemos_a_pablo");
         return view('auth.register');
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
+        $request->request->add(["username"=> Str::slug($request->username)]);//Se utiliza para que valida que no exista en la base de datos
+
         $this->validate($request, [
-            'name'=>'required | max:30',
-            'username'=>'required |unique:users|min:3|max:30',
-            'email' =>'required|unique:users|email|max:60',
-            'password' =>'required|confirmed|min:6'
+            'name' => 'required | max:30',
+            'username' => 'required |unique:users|min:3|max:30',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required|confirmed|min:6' //Con confirmed podemos encriptar la contraseña
         ]);
-        dd("Inserte los datos del formulario");
+        
+        User::create([
+            "name"=> $request->name,
+            "username"=> $request->username,
+            "email" =>$request->email,
+            "password"=> Hash::make($request->password)
+            // "password"=> bcrypt($request->password)
+        ]);
+        return redirect()->route("register.index");
+        // dd("Inserte los datos del formulario");  
     }
+    
 
 }
